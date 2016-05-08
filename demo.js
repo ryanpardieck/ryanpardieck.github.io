2f0fc5eb86dc82ffef86cee79ecf4d82a5a6f45d
Ryan Pardieck <ryan@ryanpardieck.com>
2016-05-08 09:50:30 -0400

    demo

--- demo.js
+++ demo.js
@@ -2,13 +2,25 @@
  * Created by ryanpardieck on 5/8/16.
  */
 
+var extensionId = "deogklgnkeackbmhmgdfncclfflojglo";
 
 function setChildTextNode(elementId, text) {
     document.getElementById(elementId).innerText = text;
 }
 
 
+
+
+var port = chrome.runtime.connect(extensionId, {"name": "demoPage"});
+
+chrome.runtime.sendMessage(extensionId, {"hello": "hello"},
+    function(response) {
+        if (!response.success)
+            handleError(url);
+    });
+
 function demoBeep() {
+    port.postMessage({text: "test"});
     setChildTextNode("demoBeepResults", "running...");
 }
 
