af1dc28684c5003807fd33b09be2a945b6fbe30a
Ryan Pardieck <ryan@ryanpardieck.com>
2016-05-10 14:06:30 -0400

    demo

--- demo.js
+++ demo.js
@@ -20,20 +20,16 @@ chrome.runtime.sendMessage(extensionId, {command: "beep plz"},
 function requestBeep() {
     chrome.runtime.sendMessage(extensionId, {command: "beep plz"},
         function(response) {
-            if (response.command === "beep plz"){
                 console.log("updating beep after seeing msg");
                 setChildTextNode("demoBeepResults", response.response);
-            }
         });
 }
 
 function requestSearch() {
     chrome.runtime.sendMessage(extensionId, {command: "search"},
         function(response) {
-            if (response.command === "search") {
                 console.log("updating search after seeing msg");
                 setChildTextNode("demoSearchResults", response.response);
-            }
         });
 }
 
